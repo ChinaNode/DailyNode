a79547a8cc724d50db7bfbb6f17323694578b4f9
pana <pana.wang@outlook.com>
2014-08-24 13:45:19 +0800

    small update

--- routes/index.js
+++ routes/index.js
@@ -332,9 +332,10 @@ function _replace (origin, url) {
 
 function hasContent(post) {
 	var source = ['NodeSource', 'StackOverFlow', 'StrongLoop', 'Nodejitsu', 'NPM', 'Nodejs.org']
+	var noSource = ['Venturebeat', 'EchoJS']
 	if (source.indexOf(post.source) > -1) return true
 	if (post.link.match('blog.nodejs.org')) return true
-	if (post.source == 'EchoJS') return false
+	if (noSource.indexOf(post.source) > -1) return false
 	if (post.description || post.summary) return true
 	return false
 }
