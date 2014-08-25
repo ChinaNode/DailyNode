a23a6594caf9be813bdfb95e824da316b274e95d
pana <pana.wang@outlook.com>
2014-08-25 19:36:53 +0800

    add one rss source

--- tests/rssCrawler.js
+++ tests/rssCrawler.js
@@ -3,6 +3,7 @@ var fetchRSS = require('../scripts/rssCrawler').fetchRSS
 
 var url = 'http://blog.nodejitsu.com/rss/'
 // var url = 'http://nodeweekly.com/rss'
+var url = "http://blog.nodesecurity.io/rss"
 fetchRSS(url, function (err, posts) {
     if(err)
         console.log(err)
