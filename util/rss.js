d08022fed164b755350e590ab5d0f0e7afc7f96a
pana <pana.wang@outlook.com>
2014-08-29 21:20:27 +0800

    change feed post url

--- util/rss.js
+++ util/rss.js
@@ -1,11 +1,12 @@
 var RSS = require('rss')
+var url = 'http://news.rednode.cn'
 
 exports.genRss = function (posts) {
 	var feed = new RSS({
 		title: "DailyNode -- The most timely and complete Node aggregator",
 		description: "A hub of Node things: news, blog, video, module, product",
 		feed_url: "http://news.rednode.cn/feed",
-		site_url: "http://news.rednode.cn",
+		site_url: url,
 		image_url: "http://yuan.rednode.cn/assets/img/node44.png",
 		author: "Pana W",
 		copyright: "RedNode",
@@ -15,10 +16,11 @@ exports.genRss = function (posts) {
 		categories: ['Node', 'Tech']
 	})
 	posts.forEach(function (post) {
+		var link = post.description ? url + '/item/' + post._id : post.link
 		feed.item({
 			title: post.title,
 			description: post.description,
-			url: post.link,
+			url: link,
 			author: post.author,
 			date: post.pubDate,
 			categories: [post.category]
